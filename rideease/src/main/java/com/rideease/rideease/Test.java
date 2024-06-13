149ca1ff931fc2320f2be7102ac2774fa71981c5
Abinash Mahato <ababiabi796@gmail.com>
2024-06-13 11:55:10 +0545

    java project rideease

---
+++ rideease/src/main/java/com/rideease/rideease/Test.java
@@ -0,0 +1,28 @@
+package com.rideease.rideease;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Test {
+
+    @Id
+    private Long id;
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
---
+++ rideease/src/main/java/com/rideease/rideease/TestController.java
@@ -0,0 +1,18 @@
+//package com.rideease.rideease;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//
+//public class TestController {
+//
+//    @GetMapping("/")
+//    public Test getTest() {
+//        Test test = new Test();
+//        test.setName("maal");
+//        test.setId(1L);
+//        return test;
+//    }
+//}
