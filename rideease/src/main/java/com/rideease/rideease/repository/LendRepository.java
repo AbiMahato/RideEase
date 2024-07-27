d506b186acb4866026976c251b19e4a4ccc0fcaf
Abinash Mahato <ababiabi796@gmail.com>
2024-07-27 09:22:40 +0545

    search feature implemented in vehicle list page

--- rideease/src/main/java/com/rideease/rideease/repository/LendRepository.java
+++ rideease/src/main/java/com/rideease/rideease/repository/LendRepository.java
@@ -8,9 +8,17 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface LendRepository extends JpaRepository<LendModel,Long> {
-    @Query("SELECT l FROM LendModel l WHERE l.currentLocation = :current_location AND l.vehicleType = :vehicle_type")
-    List<LendModel> findByCurrentLocationAndVehicleType(@Param("current_location")String currentLocation, @Param("vehicle_type") String vehicleType);
+    @Query("SELECT l FROM LendModel l WHERE " +
+            "(:location IS NULL OR l.currentLocation LIKE %:location%) AND " +
+            "(:vehicleType IS NULL OR l.vehicleType = :vehicleType)")
+    List<LendModel> findByLocationAndVehicleType(@Param("location") String location,
+                                                 @Param("vehicleType") String vehicleType);
 
+    @Query("SELECT l FROM LendModel l WHERE l.currentLocation LIKE %:location%")
+    List<LendModel> findByLocation(@Param("location") String location);
+
+    @Query("SELECT l FROM LendModel l WHERE l.vehicleType = :vehicleType")
+    List<LendModel> findByVehicleType(@Param("vehicleType") String vehicleType);
 
 
 }
